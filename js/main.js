79864dad87d4436670d3019999aba48873c545b8
hemomaster <67165275+hemomaster@users.noreply.github.com>
2021-04-08 21:11:45 +0300

    День 2.
    До верстал и стилизовал весь landing. Подключил и настроил swiper.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+document.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".carousel-box", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".carousel-pagination",
+      clickable: true,
+    },
+  });
 });
