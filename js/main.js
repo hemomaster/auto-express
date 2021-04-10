6dc8ad53ab3414bed28625a5aab2d3f356cb107e
hemomaster <67165275+hemomaster@users.noreply.github.com>
2021-04-10 21:07:46 +0300

    День 3. Рефакторинг и адаптив первых секцийю
    Рефакторинг кода.
    Сверстал заново и стилизовал шапку сайта.
    Адаптировал шапку и секции: наши услуги, почему мы.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // SCROLL SMOOTH
+  // к якорным ссылкам добавить класс scroll-link
+  {
+    const scrollLinks = document.querySelectorAll("a.scroll-link");
+    const scrollToElem = (el) => {
+      document.querySelector(el).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    };
+    scrollLinks.forEach((link) => {
+      link.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        scrollToElem(link.getAttribute("href"));
+      });
+    });
+  }
+
+  // CAROUSEL
   var swiper = new Swiper(".carousel-box", {
     slidesPerView: 3,
     spaceBetween: 10,
